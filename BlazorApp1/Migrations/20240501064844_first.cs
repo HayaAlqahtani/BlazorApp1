05d55e4d035c8661161e5a5b504dafb16b52619a
HayaAlqahtani <HayaAlqahtani10@gmail.com>
2024-05-01 10:12:57 +0300

    Add project files.

---
+++ BlazorApp1/Migrations/20240501064844_first.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoItems",
+                columns: new[] { "Id", "Description", "DueDate", "IsCompleted" },
+                values: new object[,]
+                {
+                    { 1, "Complete All Coded Assignments", new DateTime(2024, 4, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), false },
+                    { 2, "Clean My Room", new DateTime(2024, 4, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), true },
+                    { 3, "Apartment Rent", new DateTime(2024, 4, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), false }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
